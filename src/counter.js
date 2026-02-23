04bd07346a237f2afd2259732facec6217909676
AlexLpzsb <alex.lpzsb@gmail.com>
2026-02-23 13:54:26 +0100

    Añadido efecto de sonido

--- src/counter.js
+++ src/counter.js
@@ -1,8 +1,11 @@
 export function setupCounter(element) {
   let counter = 0;
+  const audio = new Audio('/click.mp3');
   const setCounter = (count) => {
     counter = count;
     element.innerHTML = `Count is ${counter}`;
+    audio.currentTime = 0; 
+    audio.play();
   };
   element.addEventListener('click', () => setCounter(counter + 1));
   setCounter(0);
