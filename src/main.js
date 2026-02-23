f81e50e8d6e3c7b7da0f1d5a6c29ce7c4f1261df
AlexLpzsb <alex.lpzsb@gmail.com>
2026-02-23 12:49:20 +0100

    Añadidos estilos CSS al contador

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
+import './style.css';
 import { setupCounter } from './counter.js';
 import { person, sayHello } from './modules.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Hello Vite!</h1>
+    <h1>Hello!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
